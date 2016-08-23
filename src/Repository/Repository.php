732b94d17ee75245ac4f039de49f338796121ec9
Cyrus Collier <web@cyruscollier.com>
2016-08-23 12:52:13 -0600

    Repository interface

---
+++ src/Repository/Repository.php
@@ -0,0 +1,54 @@
+<?php 
+
+namespace Spark\Repository;
+
+use Spark\Model\Collection;
+
+/**
+ * Defines basic methods for a model repository
+ * 
+ * @author cyruscollier
+ *
+ */
+interface Repository
+{
+    
+    /**
+     * Get Model instance matching unique identifier
+     * 
+     * @param mixed $id
+     * @return Model|false
+     */
+    public function findById( $id );
+    
+    /**
+     * Get Model instance matching query parameters
+     *
+     * @param mixed $id
+     * @return Model|false
+     */
+    public function findOne( $params );
+    
+    /**
+     * Get all Model instances matching query parameters
+     * 
+     * @param array $params
+     * @return Collection
+     */
+    public function find( $params = [] );
+    
+    /**
+     * Persist Model instance
+     * 
+     * @param Model $object
+     */
+    public function add( Model $object );
+    
+    /**
+     * Delete Model instance
+     * 
+     * @param Model $object
+     */
+    public function remove( Model $object );
+    
+}
