af050816354bec8257daadc01c2413d424369437
Cyrus Collier <web@cyruscollier.com>
2016-08-27 09:16:38 -0600

    added phpspec bootstrap file for minimal WP references

---
+++ tests/unit/bootstrap.php
@@ -0,0 +1,12 @@
+<?php 
+
+define('WP_PATH', 'vendor/wordpress-dev/src/');
+
+require_once WP_PATH . 'wp-includes/class-wp-post.php';
+
+/* Mocked WP functions used in domain objects */
+function apply_filters($tag, $value) { return $value; }
+function do_action($tag) {}
+
+function get_the_excerpt($post) { return $post->post_excerpt; }
+
