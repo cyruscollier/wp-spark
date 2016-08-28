7cead4260d8ca1066f769ecb743a7c65cc768308
Cyrus Collier <web@cyruscollier.com>
2016-08-28 15:47:44 -0600

    mu plugin

---
+++ src/spark.php
@@ -0,0 +1,12 @@
+<?php 
+
+/*
+ Plugin Name: WP Spark
+ Description: An expressive and elegant object-oriented API for WordPress
+ Author: Cyrus Collier
+ Version: 0.1
+ */
+$vendor_dir = dirname( dirname( __DIR__ ) );
+if ( !file_exists( $vendor_dir . '/autoload.php' ) )
+    $vendor_dir = dirname( dirname( $vendor_dir ) );
+require_once $vendor_dir . '/autoload.php';
