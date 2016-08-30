5205be1a63cec64be67a21d34da97382bf401cd0
Cyrus Collier <web@cyruscollier.com>
2016-08-29 20:23:14 -0600

    added function stub

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -9,3 +9,4 @@ function apply_filters($tag, $value) { return $value; }
 function do_action($tag) {}
 function get_the_excerpt($post) { return $post->post_excerpt; }
 function maybe_unserialize($value) { return $value; }
+function maybe_serialize($value) { return $value; }
