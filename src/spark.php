da401c50dc897c9ca919d8553939c609edf3fadd
Cyrus Collier <web@cyruscollier.com>
2016-09-01 05:26:35 -0600

    mu plugin builds container

--- src/spark.php
+++ src/spark.php
@@ -10,3 +10,5 @@ $vendor_dir = dirname( dirname( __DIR__ ) );
 if ( !file_exists( $vendor_dir . '/autoload.php' ) )
     $vendor_dir = dirname( dirname( $vendor_dir ) );
 require_once $vendor_dir . '/autoload.php';
+
+add_action( 'muplugins_loaded', 'Spark\\Container::build' );
