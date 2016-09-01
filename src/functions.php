365593bbdfbb0c70c489eeba6f438b2dbc259ce2
Cyrus Collier <web@cyruscollier.com>
2016-09-01 05:25:28 -0600

    added filter/action wrappers

---
+++ src/functions.php
@@ -0,0 +1,31 @@
+<?php
+
+use Spark\Container\Container;
+/**
+ * Global utility and convenience function
+ * 
+ * @author cyruscollier
+ *
+ */
+
+define( 'SPARK_PATH', dirname( __DIR__ ) );
+
+function spark( $name = null )
+{
+    $container = Container::getInstance();
+    return $name ? $container->get( $name ) : $container;
+}
+
+function spark_filter( $tag, $value )
+{
+    $args = func_get_args();
+    $args[0] = 'spark_' . $args[0];
+    return call_user_func_array( 'apply_filters', $args );
+}
+
+function spark_action( $tag )
+{
+    $args = func_get_args();
+    $args[0] = 'spark_' . $args[0];
+    return call_user_func_array( 'do_action', $args );
+}
